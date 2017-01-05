b9833eca1acad94b202ad8b1f58718b4ae022d20
SErAphLi <seraphlivery@gmail.com>
2017-01-05 18:21:42 +0800

    Add comments

--- GridWorld/RunGridWorld.py
+++ GridWorld/RunGridWorld.py
@@ -1,20 +1,28 @@
 from GridWorld import GridWorld, OptimalPolicy
 import matplotlib.pyplot as plt
 
+# World configuration
 world_size = (5, 5)
 special_state = [([0, 1], [4, 1], 10), ([0, 3], [2, 3], 5)]
 
+# Initialize grid world
 world = GridWorld(world_size, special_state)
 
+# Policy configuration
 policy = OptimalPolicy(0.9)
+
+# Iteration
 iteration = 0
 diffs = []
 while not (world.diff < 1e-4):
     world.step(policy)
     iteration += 1
     diffs.append(world.diff)
+
+# Show value matrix
 world.show_value(3)
 
+# Plot gradient curve
 plt.figure(1)
 plt.plot(range(iteration), diffs)
 plt.show()
